f8143094311728f83b69dd339f75ea3fb957213b
DESKTOP-ATVUOK3\bychoi00 <bychoi00@naver.com>
2018-06-02 21:59:30 +0900

    해상도 변경 / 용량 줄이기 완료

--- StockDicApp/app/src/main/java/com/bychoi00/stockdicapp/MainActivity.java
+++ StockDicApp/app/src/main/java/com/bychoi00/stockdicapp/MainActivity.java
@@ -88,7 +88,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 //Toast.makeText(getApplicationContext(), "Selected: " + position, Toast.LENGTH_SHORT).show();
-                viewPager.setCurrentItem(position);
+                try{
+                    viewPager.setCurrentItem(position,false);
+                }
+                catch (Exception ex){
+                    ex.printStackTrace();
+                }
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
